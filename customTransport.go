184d9a6dccb75626917fbd83b058e2e2d9f6d727
thomasA0509 <tallred2022@fau.edu>
2023-03-11 01:59:48 -0500

    Tweaks

--- customTransport.go
+++ customTransport.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	// "compress/gzip"
+	"compress/gzip"
 	"fmt"
 	"io/ioutil"
 	"net"
 	"net/url"
-	"time"
 
 	"github.com/DalphanDev/Turbo/http"
-
+	"github.com/andybalholm/brotli"
 	// "github.com/DalphanDev/Turbo/mimic"
-	tls "github.com/refraction-networking/utls"
 )
 
 func main() {
@@ -19,7 +17,8 @@ func main() {
 
 	// What are the steps to making a request with uTLS?
 
-	targetURL := "https://example.com/"
+	// targetURL := "https://example.com/"
+	targetURL := "https://www.whatsmybrowser.org/"
 
 	parsedURL, err := url.Parse(targetURL)
 	if err != nil {
@@ -34,15 +33,7 @@ func main() {
 
 	fmt.Println(targetAddress)
 
-	// Dial the server for a TCP connection. We need a TCP connection to create a UClient.
-	// dialer := &net.Dialer{}
-	// conn, err := dialer.Dial("tcp", targetAddress)
-
-	// Make an http transport using our custom Dial TLS function.
-
-	transport := &http.Transport{
-		// DialTLS: DialWithUTLS, // Comment this out to test uTLS vs native TLS
-	}
+	transport := &http.Transport{}
 
 	fmt.Println(transport.DialTLS)
 
@@ -50,58 +41,64 @@ func main() {
 		Transport: transport,
 	}
 
-	// config := &tls.Config{
-	// 	ServerName: serverName,
-	// 	InsecureSkipVerify: true,
-	// }
+	// First, create the HTTP request
+	req, err := http.NewRequest("GET", targetURL, nil)
+	if err != nil {
+		fmt.Printf("http.NewRequest() error: %+v\n", err)
+		return
+	}
 
-	// tlsUconn := tls.UClient(conn, config, tls.HelloChrome_72)
+	req.Header.Add("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+	req.Header.Add("accept-language", "en-US,en;q=0.9")
+	req.Header.Add("accept-encoding", "gzip, deflate, br")
+	req.Header.Add("dnt", "1")
+	req.Header.Add("sec-ch-ua", "\"Chromium\";v=\"110\", \"Not A(Brand\";v=\"24\", \"Google Chrome\";v=\"110\"")
+	req.Header.Add("sec-ch-ua-mobile", "?0")
+	req.Header.Add("sec-ch-ua-platform", "\"Windows\"")
+	req.Header.Add("sec-fetch-dest", "document")
+	req.Header.Add("sec-fetch-mode", "navigate")
+	req.Header.Add("sec-fetch-site", "none")
+	req.Header.Add("sec-fetch-user", "?1")
+	req.Header.Add("upgrade-insecure-requests", "1")
+	req.Header.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
 
-	resp, err := client.Get(targetURL)
+	// resp, err := client.Get(targetURL)
+	resp, err := client.Do(req)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
+	// body, err := ioutil.ReadAll(resp.Body)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Println(resp.Header.Get("Content-Encoding")) // Encoding type
+
+	if resp.Header.Get("Content-Encoding") == "gzip" {
+		gz, err := gzip.NewReader(resp.Body)
+		if err != nil {
+			// handle error
+		}
+		defer gz.Close()
+		body, err := ioutil.ReadAll(gz)
+		if err != nil {
+			// handle error
+		}
+		// Use body for the decompressed response
+
+		fmt.Println(string(body))
+	} else if resp.Header.Get("Content-Encoding") == "br" {
+		fmt.Println("Using br")
+
+		fmt.Println(resp.Body)
+
+		br := brotli.NewReader(resp.Body)
+		body, err := ioutil.ReadAll(br)
+		if err != nil {
+			// handle error
+		}
+		fmt.Println(body)
 	}
-
-	fmt.Println(string(body))
-}
-
-func DialWithUTLS(network, addr string) (*tls.UConn, error) {
-	// create a dialer object
-	dialer := &net.Dialer{
-		Timeout:   time.Second * 30,
-		KeepAlive: time.Second * 30,
-		DualStack: true,
-	}
-
-	// establish a TCP connection to the remote server
-	conn, err := dialer.Dial(network, addr)
-	if err != nil {
-		fmt.Println("TCP Connection Failed!")
-	}
-
-	chromeAuto := tls.HelloChrome_72
-
-	tlsConn := tls.UClient(conn, &tls.Config{
-		ServerName:         addr,
-		InsecureSkipVerify: true,
-	}, chromeAuto)
-
-	if err != nil {
-		fmt.Printf("uTLSConn generation error: %+v", err)
-	}
-
-	// perform the uTLS handshake
-	err = tlsConn.Handshake()
-	if err != nil {
-		conn.Close()
-		fmt.Println("TLS Handshake Failed!")
-	}
-
-	return tlsConn, nil
 }
